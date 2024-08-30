6372d9b385051f1615b8b7e5df88bb868ccfa0a6
Fulvia Mourikis <zum281@gmail.com>
2024-08-30 18:28:28 +0200

    feat: add slider to change grid size

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
 const grid = document.querySelector(".grid");
+const sizeInput = document.getElementById("size");
+const sizeSpanValue = document.getElementById("sizeSpan");
+
+sizeInput.value = 16;
+sizeSpanValue.textContent = `${sizeInput.value} x ${sizeInput.value}`;
+
+sizeInput.addEventListener("input", () => {
+  sizeSpanValue.textContent = `${sizeInput.value} x ${sizeInput.value}`;
+  deleteGrid();
+  createGrid(sizeInput.value);
+});
 
 var color = "#333";
 
-function createGrid(size) {
+const createGrid = (size) => {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
     square.style.width = `${100 / size}%`;
     square.style.height = `${100 / size}%`;
     grid.appendChild(square);
-  }
-}
 
-createGrid(16);
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = color;
+    });
+  }
+};
+
+const deleteGrid = () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.remove();
+  });
+};
+
+createGrid(sizeInput.value);
 
 const squares = document.querySelectorAll(".square");
-
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = color;
-  });
-});
